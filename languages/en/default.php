591979dde1086bd2b3e9dc380ec2a9a1aabfa695
Michael Gruschwitz <info@grusch-it.de>
2014-02-13 19:18:51 +0100

    Initial version

---
+++ languages/en/default.php
@@ -0,0 +1,59 @@
+<?php if (!defined('TL_ROOT')) {
+    die('You cannot access this file directly!');
+}
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Michael Gruschwitz 2014
+ * @author     Michael Gruschwitz <info@grusch-it.de>
+ * @package    Isotope
+ * @subpackage Payment
+ * @license    LGPL
+ * @filesource
+ */
+
+/**
+ * Payment modules
+ */
+$GLOBALS['ISO_LANG']['PAY']['sepa'] = array
+(
+    'SEPA',
+    'Collects bank account data from the customer.'
+);
+
+/**
+ * SEPA labels
+ */
+$GLOBALS['TL_LANG']['ISO']['sepa_holder'] = 'Account holder';
+$GLOBALS['TL_LANG']['ISO']['sepa_iban'] = 'IBAN';
+$GLOBALS['TL_LANG']['ISO']['sepa_bic'] = 'BIC';
+
+/**
+ * Error messages
+ */
+$GLOBALS['TL_LANG']['ERR']['sepa'] = array
+(
+    'iban_country' => 'Please enter an IBAN with a valid country code!',
+    'iban_length' => 'The entered IBAN is either too short or too long!',
+    'iban_invalid' => 'Please enter a valid IBAN!',
+    'bic_invalid' => 'Please enter a valid BIC!'
+);
