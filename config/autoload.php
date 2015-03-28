90ef1cfb7e0d710e9957b44f37f69a92c4563a09
Michael Gruschwitz <info@grusch-it.de>
2015-03-28 12:44:55 +0100

    Autoloading

---
+++ config/autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   isotope_payment_sepa
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL
+ * @copyright Michael Gruschwitz 2015
+ */
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+    'Gruschit',
+));
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+
+));
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+
+));
