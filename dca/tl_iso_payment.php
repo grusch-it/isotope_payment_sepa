d179df5c12aff0f882ef7e7d392815c9b9dd7050
Michael Gruschwitz <info@grusch-it.de>
2015-03-28 12:43:35 +0100

    Update dca configuration

---
+++ dca/tl_iso_payment.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   isotope_payment_sepa
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL
+ * @copyright Michael Gruschwitz 2015
+ */
+
+$GLOBALS['TL_DCA']['tl_iso_payment']['palettes']['sepa'] = '{type_legend},name,label,type;{note_legend:hide},note;{config_legend},new_order_status,minimum_total,maximum_total,countries,shipping_modules,product_types,product_types_condition,config_ids;{price_legend:hide},price,tax_class;{expert_legend:hide},guests,protected;{enabled_legend},enabled';
