f0eb230d048b0cd070f078550742b54ad66984cc
Michael Gruschwitz <info@grusch-it.de>
2015-03-29 17:13:55 +0200

    Reset config

--- config/config.php
+++ config/config.php
@@ -1,52 +1,12 @@
-<?php if (!defined('TL_ROOT')) {
-    die('You cannot access this file directly!');
-}
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2013 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
+ * Copyright (c) 2005-2015 Leo Feyer
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Michael Gruschwitz 2014
- * @author     Michael Gruschwitz <info@grusch-it.de>
- * @package    Isotope
- * @subpackage Payment
- * @license    LGPL
- * @filesource
+ * @package   isotope_payment_sepa
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL
+ * @copyright Michael Gruschwitz 2015
  */
-
-/**
- * Isotope payment modules
- */
-$GLOBALS['ISO_PAY']['sepa'] = 'PaymentSepa';
-
-/**
- * Hooks
- */
-// validation
-if (!is_array($GLOBALS['TL_HOOKS']['addCustomRegexp'])) {
-    $GLOBALS['TL_HOOKS']['addCustomRegexp'] = array();
-}
-$GLOBALS['TL_HOOKS']['addCustomRegexp'][] = array('SepaValidator', 'validate');
-
-// email data injection
-if (!is_array($GLOBALS['ISO_HOOKS']['getOrderEmailData'])) {
-    $GLOBALS['ISO_HOOKS']['getOrderEmailData'] = array();
-}
-$GLOBALS['ISO_HOOKS']['getOrderEmailData'][] = array('PaymentSepaHelper', 'onGetOrderEmailData');
